f48551af841d9d0107957d8d783758b71f3ca584
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-05-16 22:12:18 +0000

    Onbording screen static page completed

---
+++ clearpath/app/onboarding/onboardingWelcome/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export default function OnboaringWelcome() {
+  return (
+    <div className="">
+      <div className="w-full h-screen  bg-[#E2E8F0] bg-opacity-95 backdrop-blur-sm">
+        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+          <div className=" flex flex-col items-center justify-center gap-4 w-[346px] m-auto">
+            <Image
+              src="/Group.svg"
+              alt="Blurred Logo"
+              height={120}
+              width={120}
+              className="object-contain filter blur-190"
+            />
+            <div className="mx-auto items-center text-center">
+              <h2 className="text-[#101729] ">
+                Welcome, <span className="text-[#57BE73]">Samson</span>
+              </h2>
+              <p className="text-[#4A5567]">
+                A great team begins with efficiency. Create your first workspace
+                to get started
+              </p>
+            </div>
+            <Input type="email" placeholder="Workspace name" />
+            <Button variant="default">Next</Button>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
---
+++ clearpath/components/ui/input.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-10  w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm text-black  file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-gray-500 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-300 dark:placeholder:text-gray-400",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Input.displayName = "Input";
+
+export { Input };
