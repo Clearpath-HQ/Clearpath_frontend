9240ea6b1ffcc89785c034c70bd0f117ea3df4a8
Were Samson Bruno <werefast2000@gmail.com>
2024-05-16 11:17:06 +0000

    add text gray 900

--- clearpath/app/[dashboard]/terminal/page.tsx
+++ clearpath/app/[dashboard]/terminal/page.tsx
@@ -2,12 +2,8 @@
 import { useRef, useState } from "react";
 
 export default function Terminal() {
-  
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4"> 
-
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 text-gray-900"> 
     </main>
-    
   );
 }
