c86a90f02f177de1afe468e21ecb2534060e5cfd
Were Samson Bruno <werefast2000@gmail.com>
2024-05-17 13:38:22 +0000

    add icons(calendar and clock)

--- clearpath/components/ui/TaskCard.tsx
+++ clearpath/components/ui/TaskCard.tsx
@@ -12,8 +12,20 @@ const TaskCard: React.FC<TaskCardProps> = ({ date, time, description, initials }
     <div className="bg-white rounded-lg shadow-md p-4 border-l-4 border-green-500">
       <div className="flex justify-between items-center mb-2">
         <div className="flex items-center">
-          <svg className="w-5 h-5 text-gray-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m0 0a4 4 0 014 4h-4zm0 0H4a4 4 0 014-4zm8 14v-4a4 4 0 00-4-4H8m4 8a4 4 0 01-4-4m4 4v4m0-4H4a4 4 0 014-4m8 14a4 4 0 01-4-4m4 4h4a4 4 0 00-4-4m-4 0v-4m0 4h4a4 4 0 01-4 4zm8-18v4a4 4 0 01-4 4H4m8-8a4 4 0 004-4m4 0v4a4 4 0 01-4 4h-4" />
+         {/* Calendar icon */}
+         <svg
+            className="w-5 h-5 text-gray-500 mr-2"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+            />
           </svg>
           <span className="text-gray-700">{date}</span>
         </div>
@@ -22,8 +34,20 @@ const TaskCard: React.FC<TaskCardProps> = ({ date, time, description, initials }
         </div>
       </div>
       <div className="flex justify-start items-center mb-2">
-        <svg className="w-5 h-5 text-green-500 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8v4l3 3m6 1V5a2 2 0 00-2-2H5a2 2 0 00-2 2v16l4-4h11a2 2 0 002-2z" />
+        {/* Clock icon */}
+        <svg
+          className="w-5 h-5 text-green-500 mr-2"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
         </svg>
         <span className="text-green-500">{time}</span>
       </div>
