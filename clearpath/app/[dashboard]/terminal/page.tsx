5597e2e232dfb0676721949d9adb2cd9e5888cff
boypaida12 <baddysackey5@gmail.com>
2024-05-19 12:15:45 +0000

    update cards on dashboard terminal

--- clearpath/app/[dashboard]/terminal/page.tsx
+++ clearpath/app/[dashboard]/terminal/page.tsx
@@ -1,34 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
 "use client";
-import { Button } from "@/components/ui/button";
-import { BellIcon, RefreshCwIcon } from "lucide-react";
-import { useRef, useState } from "react";
+import CreateSprintCard from "@/components/component/terminal/createsprintcard";
+import DailyMotivation from "@/components/component/terminal/dailymotivation";
+import TerminalCoffeeCard from "@/components/component/terminal/terminalcoffeecard";
+import TopBar from "@/components/component/topbar";
 
 export default function Terminal() {
-  const currentDate = new Date();
-  const options: Intl.DateTimeFormatOptions = {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-  };
-  const formattedDate = currentDate.toLocaleDateString("en-US", options);
+  
+
 
   return (
     <main className="min-h-screen w-[1058px] mx-auto relative">
-      <div className="flex mt-14 after:content[''] after:absolute after:w-full after:h-[1px] after:bg-[#64748b30] after:top-[96px]">
-        <div>
-          <p className=" font-medium text-3xl">Hello, Roy</p>
-          <p className=" text-[#64748B] text-xl">{formattedDate}</p>
+      <TopBar/>
+        <div className=" flex mt-[96px]">
+          <DailyMotivation/>
+          <TerminalCoffeeCard/>
         </div>
-        <div className="flex self-center ms-auto gap-2">
-          <Button size="sm" variant="outline" className="shadow font-normal">
-            <RefreshCwIcon className="mr-2 w-4" strokeWidth="2px"/>
-            Switch
-          </Button>
-          <Button size="sm" variant="outline" className="shadow font-normal">
-            <BellIcon className="w-4" strokeWidth="2px"/>
-          </Button>
-        </div>
-      </div>
+        <p className="text-[#CBD5E1] my-6 text-xl">Sprint breakdown</p>
+        <CreateSprintCard/>
     </main>
   );
 }
---
+++ clearpath/components/component/terminal/terminalcoffeecard.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import React from "react";
+
+export default function TerminalCoffeeCard() {
+  return (
+    <div className="flex shadow-md w-[576px] h-[250px] rounded-lg ms-auto overflow-hidden relative bg-white">
+      <Image
+        src="/coffeebreak.svg"
+        alt="coffee-break"
+        width={324.84}
+        height={273.99}
+        className=" absolute top-8"
+      />
+      <div className="ms-auto pe-7 pt-7 tracking-widest text-[#CBD5E1] text-[18px] text-right capitalize">
+        <p>Today</p>
+        <p className=" text-[27.39px] mt-10 text-black font-medium">
+          Take a coffee break, <br />
+          you currently have <br /> no tasks
+        </p>
+      </div>
+    </div>
+  );
+}
